939b4b3c6348b8c7d8c574b71ec4b735ec7442f2
Hillary Le <hillaryle99@gmail.com>
2022-01-19 18:58:05 -0500

    Edited

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
 		has_one_attached :image
+		has_many_attached :pictures
 		def self.search(search)
 			where("name Like ?", "%#{search}%") #checks if the name field matches search
        end
